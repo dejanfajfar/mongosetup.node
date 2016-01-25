b15b2e3629094455ab8004141b12448e563a6d66
Dejan FAJFAR <dejan@fajfar.com>
2016-01-25 22:26:29 +0100

    Minor fixes and unit tests

--- test/MongoSetupContextFixture.js
+++ test/MongoSetupContextFixture.js
@@ -1,22 +1,142 @@
 "use strict";
 
 let assert = require('chai').assert;
+let expect = require('chai').expect;
 
 let MongoSetupContext = require('../lib/MongoSetupContext.js');
 
 describe("MongoSetupContext", () => {
     describe("constructor", () => {
-        it("If connection_string is not a string then an Error is thrown", () => {
+        it("Can not be called with undefined", () => {
             assert.throws(
-              () => new MongoSetupContext({}),
-              "The mongodb connection string has to be a non empty string");
+                () => new MongoSetupContext(undefined),
+                "Can not create a new context with undefined"
+            );
         });
 
-        it("If connection string is an empty string then an Error is thrown", () => {
+        it("MongoSetupContext.connectionString must be defined", () => {
             assert.throws(
-                () => new MongoSetupContext(""),
+              () => new MongoSetupContext({}),
+              "context data does not contain an connection string");
+        });
+
+        it("MongoSetupContext.connectionString can not be an empty string", () => {
+            assert.throws(
+                () => new MongoSetupContext({
+                    connectionString : "",
+                    db : {}
+                }),
                 "The mongodb connection string has to be a non empty string"
             );
         });
-    })
+
+        it("MongoSetupContext.db must be defined", () => {
+            assert.throws(
+                () => new MongoSetupContext({
+                    connectionString : "test",
+                    db : undefined
+                }),
+                "The mongodb connection is required"
+            );
+        });
+
+        it("MongoSetupContext.logCallback not provided then console.log used", () => {
+            let tmlConsole = console.log;
+            console.log = (message) => {
+                expect(message).to.be.equal("test message");
+            };
+
+            let context = new MongoSetupContext({
+                connectionString : "connection string",
+                db : {}
+            });
+
+            context.log("test message");
+
+            console.log = tmlConsole;
+        });
+
+        it("MongoSetupContext.logCallback not a function then console.log used", () => {
+            let tmlConsole = console.log;
+            console.log = (message) => {
+                expect(message).to.be.equal("test message");
+            };
+
+            let context = new MongoSetupContext({
+                connectionString : "connection string",
+                db : {},
+                logCallback : 35
+            });
+
+            context.log("test message");
+
+            console.log = tmlConsole;
+        });
+
+        it("MongoSetupContext.logCallback is used when provided", () => {
+            let myCallback = (message) => {
+                expect(message).to.be.equal("test message");
+            };
+
+            let context = new MongoSetupContext({
+                connectionString : "connection string",
+                db : {},
+                logCallback : myCallback
+            });
+
+            context.log("test message");
+        });
+    });
+
+    describe("getConnectionString", () => {
+        it("Returns the connection string passed to the constructor", () => {
+            let context = new MongoSetupContext({
+                connectionString : "test connection string",
+                db : {}
+            });
+
+            expect(context.getConnectionString()).to.be.equal("test connection string");
+        });
+    });
+
+    describe("closeConnection", () => {
+        it("Calls close on the DB object passed in the constructor", () => {
+            let context = new MongoSetupContext({
+                connectionString : "test",
+                db : {
+                    close : () => assert.ok(true)
+                }
+            });
+
+            context.db.close();
+        });
+
+        it("Throws error if db.close undefined or not a function", () => {
+            let context = new MongoSetupContext({
+                connectionString : "test",
+                db : {}
+            });
+
+            assert.throws(
+                () => context.db.close(),
+                ""
+            );
+        });
+    });
+
+    describe("log", () => {
+        it("Logging callback provided in constructor used", () => {
+            let context = new MongoSetupContext({
+                connectionString : "test",
+                db : {},
+                logCallback : (message) => expect(message).to.be.equal("test message")
+            });
+
+            context.log("test message");
+        });
+    });
+
+    describe("createPass", () => {
+        it()
+    });
 });
