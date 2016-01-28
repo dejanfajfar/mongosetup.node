28b208f1be88f6e7c1a234212477b1d270da9ffe
Dejan FAJFAR <dejan@fajfar.com>
2016-01-28 23:14:24 +0100

    Added mocking support and started writing unit test to check the DB promises

--- test/databaseManagerFixture.js
+++ test/databaseManagerFixture.js
@@ -1,10 +1,61 @@
 "use strict";
 
-var assert = require('chai').assert;
-var mongo_setup = require('../lib/databaseManager.js');
+var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+var assert = chai.assert;
+var expect = chai.expect;
+
+var sinon = require('sinon');
+
+var mockery = require('mockery');
+
+var mongo_setup;
+
+var validConnectionData = {
+    connectionString : "test_connection_string"
+};
 
 describe("databaseManager", () => {
+
     describe("connectTo", () => {
+
+        let connectStub = sinon.stub();
+
+        before(() => {
+
+            mockery.registerAllowables(
+                [
+                    'path',
+                    'mocha',
+                    'check-types',
+                    'chalk',
+                    'escape-string-regexp',
+                    'ansi-styles',
+                    'strip-ansi',
+                    'ansi-regex',
+                    'supports-color',
+                    '../lib/databaseManager.js',
+                    './MongoSetupContext.js',
+                    'has-ansi'
+                ]);
+
+            mockery.registerMock('mongodb', {
+                MongoClient : {
+                    connect : connectStub
+                    }
+            });
+
+            mockery.enable();
+
+            mongo_setup = require('../lib/databaseManager.js');
+        });
+
+        after(() => {
+            mockery.disable();
+        });
 
         it("connectionData must be an object", () => {
             assert.doesNotThrow(() => { mongo_setup.connectTo({
@@ -29,5 +80,36 @@ describe("databaseManager", () => {
                 () => { mongo_setup.connectTo(undefined) },
                 "connectTo can not be invoked using undefined");
         });
+
+        it("Starts the promise chain with correct data", () => {
+            connectStub.yields(undefined, {});
+            let connectToPromise = mongo_setup.connectTo(validConnectionData);
+
+            return expect(connectToPromise).to.eventually.be.fulfilled;
+
+            //mongo_setup.connectTo(validConnectionData).then(context => {
+            //    expect(context).to.be.undefined;
+            //    expect(context.connectionString).to.be.equal("test_connection_string");
+            //    expect(context.db).to.not.be.undefined;
+            //});
+        });
+
+        it("In case of connection error breaks the promise chain", () => {
+
+
+            mongo_setup.connectTo(validConnectionData).catch(reason => {
+                expect(reason).to.not.be.undefined;
+                expect(readon).to.be.an.instanceOf(Error);
+                expect(reason.message).to.be.empty;
+            });
+        });
+    });
+
+    describe("handleError", () => {
+        it("If only Error given then prints to the error stream", () => {
+
+            Promise.reject(new Error("Test Error")).catch(mongo_setup.handleError());
+        })
+
     });
 });
