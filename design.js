8e9a24535ac789633afdfb3aed6742053c2fdf69
Dejan FAJFAR <dejan@fajfar.com>
2016-02-04 23:24:16 +0100

    Removed initial design template

--- design.js
+++ design.js
@@ -1,26 +1,11 @@
-/*
-var mongoSetup = require('mongosetup');
-
-var mongo_setup = mongoSetup.forDb("mongo://localhost:27017/demo");
-
-mongo_setup.start(
-    mongo_setup.ifExistsCollection("LogEntries")
-        .then(mongo_setup.dropCollection("LogEntries"))
-        .then(mongo_setup.createCollection("LogEntries"))
-        .then(mongo_setup.fillData("LogEntries", [1, 2, 3])),
-    mongo_setup.ifNotExistsCollection("AuditTrail")
-        .then(mongo_setup.createCollection("AuditTrail"))
-).done()
-.onError(err => {});
-
-*/
-
 var mongoSetup = require('./index.js');
 var cp = mongoSetup.collectionPromises;
 
 var connectionData = {
     connectionString : "mongodb://localhost:27017/demo"
 };
+
+
 mongoSetup.connectTo(connectionData)
     .then(cp.requireVersion("1.0.0-cp1"))
     .then(cp.useCollection("MyCollection"))
@@ -31,5 +16,3 @@ mongoSetup.connectTo(connectionData)
     .then(cp.updateVersion("1.0.0-cp2"))
     .then(cp.disconnect())
     .catch(mongoSetup.handleError());
-
-
